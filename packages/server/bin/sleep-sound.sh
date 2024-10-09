5c1c6c4d511d8153ab69b1acdf43f9c8b314c462
Cody Zuschlag <cody.zuschlag@gmail.com>
2024-10-09 08:42:40 +0200

    fix hacky path to sounds

--- packages/server/bin/sleep-sound.sh
+++ packages/server/bin/sleep-sound.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
+# https://stackoverflow.com/a/246128
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 for (( i=0; i < 4; i++ ))
 do
-  mplayer -quiet -volume 150 ../../client/src/assets/cricket.mp3
+  mplayer -quiet -volume 150 $SCRIPT_DIR/../../client/src/assets/cricket.mp3
 done
